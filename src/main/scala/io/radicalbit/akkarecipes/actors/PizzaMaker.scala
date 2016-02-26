0274a300ae4158adaee30c9ebdc027b5580fc4d6
Giampaolo Trapasso <giampaolo.trapasso@radicalbit.io>
2016-02-26 14:39:13 +0100

    Organized import

--- src/main/scala/io/radicalbit/akkarecipes/actors/PizzaMaker.scala
+++ src/main/scala/io/radicalbit/akkarecipes/actors/PizzaMaker.scala
@@ -1,9 +1,7 @@
 package io.radicalbit.akkarecipes.actors
 
-import akka.actor.Actor.Receive
 import akka.actor._
-import io.radicalbit.akkarecipes.messages.MakePizza
-import io.radicalbit.akkarecipes.messages.{ MakePizza, Pizza }
+import io.radicalbit.akkarecipes.messages.{MakePizza, Pizza}
 
 class PizzaMaker extends Actor with ActorLogging {
 
