02c436b3d3cb1f6b978a0349157ed48db60402f2
Giampaolo Trapasso <giampaolo.trapasso@radicalbit.io>
2016-02-28 15:46:07 +0100

    Fixed problem with remote

--- src/main/scala/io/radicalbit/akkarecipes/PizzaRestaurantHall.scala
+++ src/main/scala/io/radicalbit/akkarecipes/PizzaRestaurantHall.scala
@@ -1,27 +1,39 @@
 package io.radicalbit.akkarecipes
 
-import akka.actor.{ActorSelection, Props, ActorSystem}
+import akka.actor.{ ActorSelection, Props, ActorSystem }
+import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
-import io.radicalbit.akkarecipes.actors.{Customer, PizzaMaker}
+import io.radicalbit.akkarecipes.actors.{ Customer, PizzaMaker }
+import io.radicalbit.akkarecipes.messages.IssueAnOrder
+import scala.concurrent.{ Await, ExecutionContextExecutor }
 import scala.concurrent.duration._
-
-import scala.concurrent.Await
 
 object PizzaRestaurantHall {
 
   def main(args: Array[String]) {
 
-    val hall: ActorSystem =
-      ActorSystem("PizzaRestaurantHall", ConfigFactory.load("PizzaRestaurantHall"))
+    val config = ConfigFactory.load("PizzaRestaurantHall")
 
-    val pizzaMaker: ActorSelection =
-      hall.actorSelection("akka.tcp://RemoteKitchen@127.0.0.2:4000/user/PizzaMaker")
+    val system = ActorSystem("SimplePizzaRestaurant", config)
 
-    val customer = hall.actorOf(Props[Customer](new Customer(pizzaMaker)), "Customer")
+    val pizzaMakerSelection: ActorSelection =
+      system.actorSelection("akka.tcp://RemoteKitchen@127.0.0.2:4000/user/PizzaMaker")
 
-    Thread.sleep(60000)
+    implicit val timeout = new Timeout(10 seconds)
+    val eventualActorRef = pizzaMakerSelection.resolveOne()
+    val pizzaMaker = Await.result(eventualActorRef, 10 seconds)
 
-    Await.ready(hall.terminate(), 10 seconds)
+    val customerName = config.getString("pizzarestaurant.people.customerName")
+    val customer = system.actorOf(Props[Customer](new Customer(pizzaMaker, customerName)), "Customer")
+
+    implicit val dispatcher: ExecutionContextExecutor = system.dispatcher
+
+    val tick = system.scheduler.schedule(500 millis, 2000 millis, customer, IssueAnOrder)
+
+    Thread.sleep(30000)
+
+    tick.cancel()
+    Await.ready(system.terminate(), 10 seconds)
     println("The restaurant is closed!")
   }
 }
--- src/main/scala/io/radicalbit/akkarecipes/RemoteKitchen.scala
+++ src/main/scala/io/radicalbit/akkarecipes/RemoteKitchen.scala
@@ -1,13 +1,12 @@
 package io.radicalbit.akkarecipes
 
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{ Props, ActorSystem }
 import com.typesafe.config.ConfigFactory
 import io.radicalbit.akkarecipes.actors.PizzaMaker
 
-object RemoteKitchen  {
+object RemoteKitchen {
 
-
-  def  main (args: Array[String]) {
+  def main(args: Array[String]) {
     val kitchen = ActorSystem("RemoteKitchen", ConfigFactory.load("RemoteKitchen"))
 
     val pizzaMaker = kitchen.actorOf(Props[PizzaMaker], "PizzaMaker")
