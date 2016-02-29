db5922de76b715360a54604117f2f5795586608c
Giampaolo Trapasso <giampaolo.trapasso@radicalbit.io>
2016-02-29 16:51:31 +0100

    Journal implemented 

--- build.sbt
+++ build.sbt
@@ -11,6 +11,9 @@ scalaVersion := "2.11.7"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.4.0",
   "com.typesafe.akka" % "akka-slf4j_2.11" % "2.4.0",
+  "com.typesafe.akka" %% "akka-persistence" % "2.4.0",
+  "org.iq80.leveldb" % "leveldb" % "0.7",
+  "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8",
   "ch.qos.logback" % "logback-classic" % "1.1.3"
 )
 
--- src/main/scala/io/radicalbit/akkarecipes/actors/PizzaMaker.scala
+++ src/main/scala/io/radicalbit/akkarecipes/actors/PizzaMaker.scala
@@ -1,23 +1,38 @@
 package io.radicalbit.akkarecipes.actors
 
+import java.util.Date
+
 import akka.actor._
+import akka.persistence.PersistentActor
 import io.radicalbit.akkarecipes.messages.{ YouOweMe, HowMuch, MakePizza, Pizza }
 
 case class Counter(number: Int)
 
-class PizzaMaker extends Actor with ActorLogging {
+case class MakePizzaEvent(pizzaOrder: Int, time: Date)
+
+class PizzaMaker extends PersistentActor with ActorLogging {
 
   var counter = Counter(0)
   val euro = 3
 
-  override def receive: Receive = {
-    case MakePizza(number) => {
-      log.info("Received order #{}", number)
-      Thread.sleep(500)
-      val p = Pizza(number)
-      sender ! p
+  override def persistenceId: String = "PizzaMaker1"
+
+  override def receiveRecover: Receive = {
+    case MakePizzaEvent(pizzaOrder, time) => {
       counter = Counter(counter.number + 1)
-      log.info("{} is ready", p)
+    }
+  }
+
+  override def receiveCommand: Receive = {
+    case MakePizza(number) => {
+      persist(MakePizzaEvent(number, new Date)) { (event: MakePizzaEvent) =>
+        log.info("Received order #{}", number)
+        Thread.sleep(500)
+        val p = Pizza(number)
+        sender ! p
+        counter = Counter(counter.number + 1)
+        log.info("{} is ready", p)
+      }
     }
     case HowMuch => {
       val oweMe = YouOweMe(counter.number * euro)
